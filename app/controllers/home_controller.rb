4794726370afee54fc7b4cc1e1da9448cd6b95d4
Sander de Winter <sanderdewinter@gamil.com>
2015-12-06 12:25:46 +0100

    Specs parsed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,14 @@ require 'json'
 class HomeController < ApplicationController
   def get_components_structure
     result = Net::HTTP.get(URI.parse('http://95.85.12.99:6543/category'))
-    @comps = JSON.parse(result)['categories']
+    @components = JSON.parse(result)['categories']
 
-    render json: @comps
+    @components.each_with_index do |component, index|
+      if index != 3
+        @components[index]['product_schema'] = JSON.parse(component['product_schema'])
+      end
+    end
+
+    render json: @components
   end
 end
