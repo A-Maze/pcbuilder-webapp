d4074335951329bd814cfac1e501cbf69b2c8e70
Davey <davey.witter@live.nl>
2016-01-31 18:06:20 +0100

    search bijna done

--- app/assets/javascripts/search_dashboard.js
+++ app/assets/javascripts/search_dashboard.js
@@ -1,15 +1,15 @@
 pcbuilder = angular.module('pcbuilder', []);
 
 pcbuilder.controller("searchController", ['$scope', '$http', function ($scope, $http) {
-	$http.get("/api/search/cpu").success(function (data) {
+	var pathArray = window.location.pathname.split( '/' );
+	var path = pathArray[pathArray.length-1];
+
+	$http.get("/api/search/"+path).success(function (data) {
 		$scope.products = data;
 		$scope.formData = {};
-		$scope.params = $http.zoekterm;
+		$scope.params = path;
 	});
-	$scope.add = function(searchTag) {
-		$http.get("/api/search/"+searchTag).success(function (data) {
-			$scope.products = data;
-			$scope.formData = {};
-		});
-	}
+	$scope.submitform = function () {
+		window.location.href = $scope.searchTag;
+    }
 }]);
